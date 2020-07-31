5c5e2b84dbbcfa5fa1c13543a99451022f9e68ec
tomsarry <55455075+tomsarry@users.noreply.github.com>
2020-07-31 12:16:43 +0200

    added share twitter button

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,6 +1,8 @@
 import React from "react";
+import twitterSVG from "./twitter.svg";
 
 const Quote = (props) => {
+  const link = `https://twitter.com/intent/tweet?text=${props.quote}%20-${props.author}`;
   return (
     <div className="div-quote">
       <h1 className="content">{props.quote}</h1>
@@ -8,6 +10,15 @@ const Quote = (props) => {
       <button className="btn-quote" onClick={props.handleClick}>
         Get a new Quote
       </button>
+      <a className="twitter-share-button" href={link}>
+        <img
+          className="svg"
+          id="share"
+          src={twitterSVG}
+          alt="share it on twitter"
+        ></img>
+        <label for="share">Share this quote on Twitter</label>
+      </a>
     </div>
   );
 };
